69afd152360ee2407b189442babeca6256823dda
天毛 瞬 <shun@amake-no-MacBook-Air.local>
2016-05-13 20:40:47 +0900

    first commit

---
+++ COUNT22/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  COUNT22
+//
+//  Created by 天毛 瞬 on 2015/06/26.
+//  Copyright (c) 2015年 天毛 瞬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
+   
+}
+    
+    
+
+
+
+
